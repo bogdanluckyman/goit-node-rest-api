8ddb1b4122421f1f57385548f07363d8ef4b484a
Bogdan <bogdantisenko06@gmail.com>
2024-02-01 15:17:01 +0200

    work

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,77 @@
 import contactsService from "../services/contactsServices.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
+import { json } from "express";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  try {
+    const contacts = await contactsService.listContacts();
+    res.status(200).json(contacts);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+  try {
+    const oneContact = await contactsService.getContactById(req.params.id);
+    if (oneContact) {
+      res.status(200).json(oneContact);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res) => {
+  try {
+    const deletedContact = await contactsService.removeContact(req.params.id);
+    if (deleteContact) {
+      res.status(200).json(deleteContact);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res) => {
+  try {
+    const validateContact = createContactSchema.validate(req.body);
 
-export const updateContact = (req, res) => {};
+    if (validateContact.error) {
+      return res.status(400).json({ message: validationResult.error.message });
+    }
+    const { name, email, phone } = req.body;
+    const newContact = await contactsService.addContact(name, email, phone);
+    res.status(201).json(newContact);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateContact = async (req, res) => {
+  try {
+    const contactId = req.params.id;
+    const updatedData = req.body;
+    const validatedContacts = updateContactSchema.validate(updatedData);
+    if (validatedContacts.error) {
+      return res.status(400).json({ message: validationResult.error.message });
+    }
+    const updateContact = await contactsService.updateContacts(
+      contactId,
+      updatedData
+    );
+    if (!updateContact) {
+      res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json(updateContact);
+  } catch (error) {
+    console.log(error);
+  }
+};
