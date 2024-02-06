044dc340c2b0f2ce2a71c273a51f31dda96979d4
Bogdan <bogdantisenko06@gmail.com>
2024-02-06 22:14:11 +0200

    finish

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -6,16 +6,16 @@ const {
 const json = require("express");
 const Contact = require("../model/contacts.js");
 
-const getAllContacts = async (req, res) => {
+const getAllContacts = async (req, res, next) => {
   try {
     const result = await Contact.find();
     res.json(result);
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 };
 
-const getOneContact = async (req, res) => {
+const getOneContact = async (req, res, next) => {
   try {
     const { id } = req.params;
     const result = await Contact.findById(id);
@@ -25,12 +25,13 @@ const getOneContact = async (req, res) => {
       res.status(404).json({ message: "Not found" });
     }
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 };
 
-const deleteContact = async (req, res) => {
+const deleteContact = async (req, res, next) => {
   try {
+    const { id } = req.params;
     const deletedContact = await Contact.findByIdAndDelete(id);
     if (deletedContact) {
       res.status(200).json(deletedContact);
@@ -38,44 +39,72 @@ const deleteContact = async (req, res) => {
       res.status(404).json({ message: "Not found" });
     }
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 };
 
 const createContact = async (req, res, next) => {
   try {
-    const result = await Book.create(req.body);
+    const result = await Contact.create(req.body);
     res.status(201).json(result);
   } catch (error) {
     next(error);
   }
 };
 
-const updateContact = async (req, res) => {
+const updateContact = async (req, res, next) => {
   try {
-    const contactId = req.params.id;
-    const updatedData = req.body;
-
-    if (Object.keys(updatedData).length === 0) {
-      return res
-        .status(400)
-        .json({ message: "Body must have at least one field" });
-    }
-
-    const validatedContacts = updateContactSchema.validate(updatedData);
-    if (validatedContacts.error) {
-      return res.status(400).json({ message: validatedContacts.error.message });
-    }
-    const updateContact = await contactsService.updateContacts(
-      contactId,
-      updatedData
-    );
-    if (!updateContact) {
+    //-----це щоб ви перевірили що я минулого разу все виправив після прийняття домашки😊
+    // const contactId = req.params.id;
+    // const updatedData = req.body;
+    // if (Object.keys(updatedData).length === 0) {
+    //   return res
+    //     .status(400)
+    //     .json({ message: "Body must have at least one field" });
+    // }
+    // const validatedContacts = updateContactSchema.validate(updatedData);
+    // if (validatedContacts.error) {
+    //   return res.status(400).json({ message: validatedContacts.error.message });
+    // }
+    // const updateContact = await Contact.findByIdAndUpdate({
+    //   contactId,
+    //   updatedData,
+    // });
+    // if (!updateContact) {
+    //   res.status(404).json({ message: "Not found" });
+    // }
+    // res.status(200).json(updateContact);
+    const { id } = req.params;
+    const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+    if (!result) {
       res.status(404).json({ message: "Not found" });
     }
-    res.status(200).json(updateContact);
+    res.json(result);
   } catch (error) {
-    console.log(error);
+    next(error);
+  }
+};
+
+const updateStatusContact = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const { favorite } = req.body;
+    if (favorite === undefined || typeof favorite !== "boolean") {
+      return res
+        .status(400)
+        .json({ message: "Invalid 'favorite' value. It must be a boolean." });
+    }
+    const updatedContact = await Contact.findByIdAndUpdate(
+      contactId,
+      { favorite },
+      { new: true }
+    );
+    if (!updatedContact) {
+      return res.status(404).json({ message: "Contact not found." });
+    }
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    next(error);
   }
 };
 
@@ -85,4 +114,5 @@ module.exports = {
   deleteContact,
   createContact,
   updateContact,
+  updateStatusContact,
 };
